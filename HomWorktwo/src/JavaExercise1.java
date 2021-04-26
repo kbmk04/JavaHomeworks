0f1adac09722ab457bf421e719431e6059475b5b
kb <kb@DESKTOP-E18V00S.lan>
2021-04-25 22:57:10 -0500

    Home Work 2

---
+++ HomWorktwo/src/JavaExercise1.java
@@ -0,0 +1,13 @@
+
+public class JavaExercise1 {
+
+	public static void main(String[] args) {
+		
+		
+		String firstName = "Tesfu";
+		String lastName = "Abay";
+		System.out.println(firstName + " " + lastName);
+			
+	}
+
+}
